d330390bb1f0e58bd0f6b09f37a4a028c62d0067
yasmeentawileh <yasmeen.tawileh@gmail.com>
2020-08-26 18:57:21 +0300

    for loop

--- contactus.js
+++ contactus.js
@@ -3,33 +3,27 @@ var age = 24;
 var homeaddres = "amman";
 var gender
 var day = new Date();
-var true1 =true; 
-console.log ('the name',name);
-console.log('the age',age);
-console.log('home',homeaddres);
-console.log('the gender',gender);
-console.log('the day',day);
-console.log('the status',true1);
- var intersted=prompt('are you interested in handcrafts?')
-if (intersted=='yes'){
+var true1 = true;
+console.log('the name', name);
+console.log('the age', age);
+console.log('home', homeaddres);
+console.log('the gender', gender);
+console.log('the day', day);
+console.log('the status', true1);
+var intersted = prompt('are you interested in handcrafts?')
+if (intersted == 'yes') {
     alert('welcome to the website')
-}else{
+} else {
     alert('see the website and you will change your opinion')
 }
 
 
-var gender = function() {
-    var userinput=prompt('what is your gender?' );
-    if(userinput=="female"){
-    alert('we have a beautiful bags for you')
-}else{
-    document.write("<h3>we have a beautiful wallets for you</h3>")
-}
-}
-
-
-
-
-
-
-
+var gender = function () {
+    var userinput = prompt('what is your gender?');
+    while (userinput != "female" && userinput != "male") {
+        console.log('gender', userinput)
+         userinput = prompt('you have to choose female or male')
+    }}
+    
+        
+        
